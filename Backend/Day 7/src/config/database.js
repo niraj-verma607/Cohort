6573bbf28a76173aa9926ca18217241fe000657d
niraj-verma607 <nirajverma9607@gmail.com>
2026-07-19 21:50:44 +0530

    Refactor database connection to use environment variable for MongoDB URI

--- Backend/Day 7/src/config/database.js
+++ Backend/Day 7/src/config/database.js
@@ -1,13 +1,9 @@
 const mongoose = require("mongoose");
 
 function connectToDb() {
-  mongoose
-    .connect(
-      "mongodb+srv://nirajverma9607_db_user:UnhOhJThtdyknc2Z@cluster0.uehaqa3.mongodb.net/",
-    )
-    .then(() => {
-      console.log("Connected to DB");
-    });
+  mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("Connected to DB");
+  });
 }
 
 module.exports = connectToDb;
