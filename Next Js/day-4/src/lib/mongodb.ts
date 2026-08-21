b29fee77f1ecaddf0dff7d1693577fe6c42e088d
niraj-verma607 <nirajverma9607@gmail.com>
2026-08-21 21:42:20 +0530

    Resume Builder | Day 1

--- Next Js/day-4/src/lib/mongodb.ts
+++ Next Js/day-4/src/lib/mongodb.ts
@@ -1,9 +1,5 @@
 import mongoose from "mongoose";
 
-import dns from "dns";
-
-dns.setServers(["8.8.8.8", "1.1.1.1"]);
-
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI!);
