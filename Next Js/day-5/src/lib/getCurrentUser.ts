8197d12949e75e78022aebd8ea961d70a04d7274
niraj-verma607 <nirajverma9607@gmail.com>
2026-08-24 22:19:02 +0530

    Next JS Resume Builder

--- Next Js/day-5/src/lib/getCurrentUser.ts
+++ Next Js/day-5/src/lib/getCurrentUser.ts
@@ -1,3 +1,7 @@
-async function getCurrentUser(){
+import { cookies } from "next/headers";
+
+async function getCurrentUser() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value;
     
 }
